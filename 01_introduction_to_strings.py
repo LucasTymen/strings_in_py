2f64c84e755c2a60dffff4d3d2de252930177d70
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-25 02:11:21 +0200

    strings_1

---
+++ 01_introduction_to_strings.py
@@ -0,0 +1,48 @@
+"""
+
+Introduction to Strings
+
+Words and sentences are fundamental to how we communicate, so it follows that we’d want our computers to be able to work with words and sentences as well.
+
+In Python, the way we store something like a word, a sentence, or even a whole paragraph is as a string. A string is a sequence of characters contained within a pair of 'single quotes' or "double quotes". A string can be any length and can contain any letters, numbers, symbols, and spaces.
+
+In this lesson, we will learn more about strings and how they are treated in Python. We will learn how to slice strings, select specific characters from strings, search strings for characters, iterate through strings, and use strings in conditional statements.
+
+Let’s get started.
+Instructions
+1.
+
+Save your favorite word as a string to the variable favorite_word.
+Checkpoint 2 Passed
+
+Strings are written inside either single quotes ' ' or double quotes " ". Just be sure they match!
+"""
+# Valid string using double quotes
+favorite_word = "coding"
+
+# Also valid string using single quotes
+favorite_word = 'coding'
+"""
+2.
+
+Print favorite_word.
+Checkpoint 3 Passed
+
+It should look something like:
+"""
+print(string_name)
+"""
+Notice how the quotes don’t appear in the output terminal.
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+Community Forums
+
+Here are some helpful links to the top questions asked by coders about this exercise:
+
+    Does saying “sequence of characters” when describing a string imply some order?
+
+Still have questions? View this exercise's thread in the Codecademy Forums.
+"""
+favorite_word = "Metal"
+
+print(favorite_word)
