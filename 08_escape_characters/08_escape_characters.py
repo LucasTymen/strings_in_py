a81f562915e06dc44432cd4a6b4980417e1f04e7
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-25 18:04:10 +0200

    strings_8

---
+++ 08_escape_characters/08_escape_characters.py
@@ -0,0 +1,39 @@
+"""
+
+Introduction to Strings
+Escape Characters
+
+Occasionally when working with strings, you’ll find that you want to include characters that already have a special
+meaning in python. For example let’s say I create the string
+
+ favorite_fruit_conversation = "He said, "blueberries are my favorite!""
+
+We’ll have accidentally ended the string before we wanted to by including the " character. The way we can do this is by
+introducing escape characters. By adding a backslash in front of the special character we want to escape, \", we can
+include it in a string.
+"""
+favorite_fruit_conversation = "He said, \"blueberries are my favorite!\""
+"""
+Now it works!
+Instructions
+1.
+
+When Rob Daily (remember him? From the last exercise?) set up his account he set his password to be
+
+theycallme"crazy"91
+
+His password was causing some errors in the system because of the " marks. Rewrite his password using escape characters
+and save it to the variable password.
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+Community Forums
+
+Here are some helpful links to the top questions asked by coders about this exercise:
+
+    How are escape characters counted towards the length of a string?
+
+What if I want a backslash in my string?
+
+Still have questions? View this exercise's thread in the Codecademy Forums.
+
+"""
