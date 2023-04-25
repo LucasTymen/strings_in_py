4c6f2a3b0d2168e4d3fbed95c0c1b43868c46330
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-25 03:52:41 +0200

    strings_4

--- 04_Concatenating_Strings/script.py
+++ 04_Concatenating_Strings/script.py
@@ -1,2 +1,9 @@
 first_name = "Julie"
 last_name = "Blevins"
+new_account_name =[]
+def account_generator(first_name, last_name):
+  new_account_name = first_name[:3]+last_name[:3]
+  return new_account_name
+
+new_account = account_generator("Julie","Blevins")
+print(new_account)
